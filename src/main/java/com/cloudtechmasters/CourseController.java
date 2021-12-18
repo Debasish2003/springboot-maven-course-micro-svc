19e2e78f5d5e77b7b6beab7a38ae051c64e8c4f2
Debasish2003 <66173758+Debasish2003@users.noreply.github.com>
2021-12-18 14:52:02 +0530

    Update CourseController.java

--- src/main/java/com/cloudtechmasters/CourseController.java
+++ src/main/java/com/cloudtechmasters/CourseController.java
@@ -11,11 +11,11 @@ public class CourseController {
 //create two endpoints getAllAwsServices and getAllDevopsTools
     @GetMapping("/getAllAwsServices")
     public List<String> getAllAwsServices(){
-        return Arrays.asList("ec2","iam","rds","ecr","eks");
+        return Arrays.asList("ec2","iam","rds","ecr","eks","test");
     }
 
     @GetMapping("/getAllDevopsTools")
     public List<String> getAllDevopsTools(){
-        return Arrays.asList("git","maven","sonar","nexus","jenkins");
+        return Arrays.asList("git","maven","sonar","nexus","jenkins","test1");
     }
 }
